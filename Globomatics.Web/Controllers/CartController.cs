bb9d3225c085e858478b02093825ad073ab1dc6b
TUSHYA TRIVEDI <ttushya@gamil.com>
2024-12-10 20:44:50 +0530

    Add to cart functionality added, view created for order details

--- Globomatics.Web/Controllers/CartController.cs
+++ Globomatics.Web/Controllers/CartController.cs
@@ -1,19 +1,44 @@
-﻿using Globomatics.Web.Models;
+﻿using Globomantics.Domain.Models;
+using Globomatics.Infrastructure.Repositories;
+using Globomatics.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Globomatics.Web.Controllers;
 
+[Route("[controller]")]
 public class CartController : Controller
 {
+    private readonly ILogger<CartController> logger;
+    private readonly ICartRepository cartRepository;
+
+    public CartController(ILogger<CartController> logger, ICartRepository cartRepository)
+    {
+        this.logger = logger;
+        this.cartRepository = cartRepository;
+    }
+
+    [Route("Index")]
     public IActionResult Index(Guid? id)
     {
         return View();
     }
 
     [HttpPost]
+    [Route("Add")]
     public IActionResult AddToCart(AddToCartModel addToCartModel)
     {
-        throw new NotImplementedException();
+        if (addToCartModel.Product is null)
+        {
+            return BadRequest();
+        }
+
+        logger.LogInformation($"Attempting to add {addToCartModel.Product} to cart {addToCartModel.CartId}");
+
+        var cart = cartRepository.CreateOrUpdate(addToCartModel.CartId, addToCartModel.Product.ProductId);
+
+        cartRepository.SaveChanges();
+
+        return RedirectToAction("Index", "Cart");
     }
 
     [HttpPost]
@@ -23,11 +48,13 @@ public class CartController : Controller
     }
 
     [HttpPost]
+    [Route("Finalize")]
     public IActionResult Create(CreateOrderModel createOrderModel)
     {
         throw new NotImplementedException();
     }
 
+    [HttpGet("ThankYou")]
     public IActionResult ThankYou()
     {
         return View();
