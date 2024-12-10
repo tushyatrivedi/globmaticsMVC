3fbf18c201008a8a912af1c57963562a26e3ca1e
TUSHYA TRIVEDI <ttushya@gamil.com>
2024-12-10 17:48:10 +0530

    custom anchoor tag helper added

---
+++ Globomatics.Web/TagHelpers/SlugTagHelper.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+
+namespace Globomatics.Web.TagHelpers
+{
+    [HtmlTargetElement("url-with-slug")]
+    public class SlugTagHelper : AnchorTagHelper
+    {
+        public SlugTagHelper(IHtmlGenerator generator) : base(generator)
+        {
+        }
+
+        [HtmlAttributeName("for-product-id")]
+        public Guid ProductId { get; set; }
+
+        [HtmlAttributeName("for-ticket-name")]
+        public required string TicketTitle { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.TagName = "a";
+
+            var slug = Regex.Replace(TicketTitle, @"[^a-zA-Z0-9]", "-", RegexOptions.CultureInvariant, TimeSpan.FromMilliseconds(200))
+                .ToLowerInvariant()
+                .Trim('-');
+
+            RouteValues.Add("productId",ProductId.ToString());
+            RouteValues.Add("slug",TicketTitle);
+
+            base.Process(context, output);
+        }
+    }
+}
