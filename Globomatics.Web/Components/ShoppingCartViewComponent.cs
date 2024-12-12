ac872655762d142b03fed3bbe9050ed4c37f14a9
TUSHYA TRIVEDI <ttushya@gamil.com>
2024-12-12 14:59:28 +0530

    added shopping card view component and calling it from CardIndex and HomeIndex views

---
+++ Globomatics.Web/Components/ShoppingCartViewComponent.cs
@@ -0,0 +1,47 @@
+﻿using Globomantics.Infrastructure.Data;
+using Globomatics.Infrastructure.Repositories;
+using Globomatics.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Globomatics.Web.Components
+{
+    public class ShoppingCartViewComponent:ViewComponent
+    {
+        private readonly GlobomanticsContext context;
+        private readonly IStateRepository stateRepository;
+        public ShoppingCartViewComponent(GlobomanticsContext context, IStateRepository stateRepository)
+        {
+            this.context = context;
+            this.stateRepository = stateRepository;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string cartId,bool isCompact)
+        {
+            if (!Guid.TryParse(cartId,out var id))
+            {
+                return View(new ShoppingCartModel
+                { 
+                    IsCompact=isCompact
+                });
+            }
+            var cart = await context.Carts
+                .Include(cart => cart.LineItems)
+                .ThenInclude(item => item.Product).
+                FirstOrDefaultAsync(cart => cart.CartId == id);
+
+            //why are we setting session variables here?
+            //if (cart is not null)
+            //{
+            //    stateRepository.SetValue("NumberOfItems",
+            //        cart.LineItems.Sum(x => x.Quantity).ToString());
+
+            //    stateRepository.SetValue("CartId",
+            //        cart.CartId.ToString());
+            //}
+
+            return View(new ShoppingCartModel { Cart=cart,IsCompact=isCompact});
+        }
+
+    }
+}
